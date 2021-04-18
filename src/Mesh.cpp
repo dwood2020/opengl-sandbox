6512d7809bbc6cd4f888486fb654384a9a6fd710
David <d.woodward@gmx.de>
2021-04-19 00:03:34 +0200

    started implementing Mesh class

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,2 +1,17 @@
 #include "Mesh.h"
 
+Mesh::Mesh(std::vector<glm::vec3>& vertices, std::vector<unsigned int>& indices):
+	vao(0), vbo(0), ebo(0), glMode(0) { 
+		
+	serializedData.resize(vertices.size() * 3);
+
+	for (unsigned int i = 0; i < vertices.size(); i++) {
+		serializedData[3*i] = vertices[i].x;
+		serializedData[3*i + 1u] = vertices[i].y;
+		serializedData[3*i + 2u] = vertices[i].z;
+	}
+
+}
+
+
+Mesh::~Mesh() { }
--- src/Mesh.h
+++ src/Mesh.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include "glad/glad.h"
+#include <glm/vec3.hpp>
 
 
 class Mesh {
@@ -8,8 +9,18 @@ public:
 	unsigned int vao;
 	unsigned int vbo;
 	unsigned int ebo;
+	GLenum glMode;
 
-	Mesh();
+protected:
+	std::vector<float> serializedData;
+	std::vector<unsigned int> indices;
+
+	Mesh(std::vector<glm::vec3>& vertices, std::vector<unsigned int>& indices);
+	//TODO: Further overloaded ctors for normals & texture coords
+	virtual ~Mesh();
+
+	void Prepare(std::vector<float> data);
+	
 };
 
 
