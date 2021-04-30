b7dba0012d6892c0f61ec3dfc5b7dc29b4471fbc
David <d.woodward@gmx.de>
2021-04-30 16:11:38 +0200

    started implementing texture class

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -2,7 +2,42 @@
 #include <stb/stb_image.h>
 
 
-Texture::Texture() { }
+Texture::Texture(): id(0), w(0), h(0), format(0) { }
 
 
 Texture::~Texture() { }
+
+
+void Texture::Generate(unsigned char* data) {
+	
+}
+
+
+
+unsigned char* Texture::LoadData(const std::string& filepath) {
+	return nullptr;
+}
+
+
+GLenum Texture::GetImageFormat(const std::string& filepath) {
+	size_t posEnding = filepath.find_last_of('.');
+
+	std::string ending = filepath.substr(posEnding + 1u);
+	for (size_t i = 0; i < ending.size(); ++i) {
+		ending[i] = std::tolower(ending[i]);
+	}
+	//TODO: catch exceptions here! (in case string is no ascii string etc...)
+
+	if (ending.compare("png")) {
+		return GL_RGBA;
+	}
+	else if (ending.compare("jpg") == 0 || ending.compare("jpeg") == 0) {
+		return GL_RGB;
+	}	
+	else {
+		// assume no alphas by default
+		//TODO: different image formats
+		return GL_RGB;
+	}
+
+}
--- src/Texture.h
+++ src/Texture.h
@@ -1,13 +1,26 @@
 #pragma once
 #include "glad/glad.h"
+#include <string>
 
 
 class Texture final {
 public:
 	GLuint id;
 
-public:
+protected:
+	GLuint w;
+	GLuint h;
+	GLenum format;
+
+public:	
 	Texture();
 	~Texture();
+
+	void Generate(unsigned char* data);
+
+	static unsigned char* LoadData(const std::string& filepath);
+
+private:
+	static GLenum GetImageFormat(const std::string& filepath);
 
 };
