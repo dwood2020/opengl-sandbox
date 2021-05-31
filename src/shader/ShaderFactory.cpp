13623f0ab26bdc37fbd0177e395ec9d3e9f78260
David <d.woodward@gmx.de>
2021-05-31 12:56:57 +0200

    started adding a shaderfactory

--- src/shader/ShaderFactory.cpp
+++ src/shader/ShaderFactory.cpp
@@ -1,1 +1,15 @@
 #include "ShaderFactory.h"
+
+
+ShaderFactory::ShaderFactory() { }
+
+
+ShaderFactory::~ShaderFactory() { }
+
+
+std::unique_ptr<ShaderProgram> ShaderFactory::MakePhongShaderProgram(void) const {
+	
+	
+	return std::unique_ptr<ShaderProgram>();
+}
+
--- src/shader/ShaderFactory.h
+++ src/shader/ShaderFactory.h
@@ -1,3 +1,18 @@
 #pragma once
+#include <memory>
+#include <string>
+#include "ShaderProgram.h"
 
+//TODO: Might move this into the material system later? &rename to "MaterialShaderFactory" or so
 
+class ShaderFactory final {
+private:
+	const std::string phongVertSourePath = "res/phong_material_vert.glsl";
+	const std::string phongFragSourcePath = "res/phong_material_frag.glsl";
+
+public:
+	ShaderFactory();
+	~ShaderFactory();
+
+	std::unique_ptr<ShaderProgram> MakePhongShaderProgram(void) const;
+};
