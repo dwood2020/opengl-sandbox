e91b7121a145925b7a2c5c3c54f56f043dcd4901
David <d.woodward@gmx.de>
2021-05-31 20:49:22 +0200

    added classes for different approaches

---
+++ src/material/MaterialUniform.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+/// <summary>
+/// This class represents a "Material uniform"
+/// </summary>
+class MaterialUniform final {
+public:
+	MaterialUniform();
+	~MaterialUniform();
+};
+
---
+++ src/material/UniformRef.cpp
@@ -0,0 +1,1 @@
+#include "UniformRef.h"
