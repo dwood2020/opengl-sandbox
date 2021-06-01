ada4ab5bafca0d1dc2de239edba3b1cf7807b4f9
dwood <d.woodward@gmx.de>
2021-06-01 19:40:51 +0200

    started implementing Uniform class

--- src/Uniform.cpp
+++ src/Uniform.cpp
@@ -1,1 +1,28 @@
 #include "Uniform.h"
+
+
+Uniform::Uniform(int value) : value(value) { }
+
+
+Uniform::Uniform(float value): value(value) { }
+
+
+Uniform::Uniform(const glm::vec3& value): value(value) { }
+
+
+Uniform::Uniform(const glm::mat4& value): value(std::make_shared<glm::mat4>(value)) { }
+
+
+UniformType Uniform::GetType(void) const {
+	return (UniformType)value.index();
+}
+
+
+int Uniform::GetInt(void) const {
+	return std::get<(size_t)UniformType::Int>(value);
+}
+
+
+float Uniform::GetFloat(void) const {
+	return std::get<float>(value);
+}
--- src/Uniform.h
+++ src/Uniform.h
@@ -1,6 +1,53 @@
 #pragma once
+#include <variant>
+#include <memory>
+#include "glm/glm.hpp"
 
 
-class Uniform final {
-
+/// <summary>
+/// This enum represents all possible uniform data types.
+/// It is equivalent with the variant fields in the Uniform class.
+/// </summary>
+enum class UniformType {
+	Int,
+	Float,
+	Vec3,
+	Mat4
 };
+
+
+/// <summary>
+/// This class represents a uniform.
+/// Approach:
+/// To compact memory a little, this union-like approach is applied.
+/// "Small" uniform values are part of the variant directly vs.
+/// "large" uniform values, e.g. mat4 are saved outside the variant.
+/// 
+/// This is a trade-off between memory usage and fragmentation optimization.
+/// </summary>
+class Uniform final {
+private:
+	std::variant<int, float, glm::vec3, std::shared_ptr<glm::mat4>> value;
+
+private:
+	Uniform() = default;
+
+public:
+	Uniform(int value);
+
+	Uniform(float value);
+
+	Uniform(const glm::vec3& value);
+
+	Uniform(const glm::mat4& value);
+
+
+	UniformType GetType(void) const;
+
+
+	int GetInt(void) const;
+
+
+	float GetFloat(void) const;
+};
+
