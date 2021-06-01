d008780feee420865c7d59e9e7653c33e6d0e66f
David <d.woodward@gmx.de>
2021-06-01 21:48:52 +0200

    added map

--- src/MaterialBase.h
+++ src/MaterialBase.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <map>
+#include <string>
 #include "Uniform.h"
 
 
 class MaterialBase {
 protected:
-
+	std::map<std::string, Uniform> uniforms;
 
 public:
 	MaterialBase() = default;
