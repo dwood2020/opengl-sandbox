a8dd6282e228e5d58e3387cde11e1a331f377e83
David <d.woodward@gmx.de>
2021-06-10 23:25:05 +0200

    first ideas

--- src/scene/OctreeManager.cpp
+++ src/scene/OctreeManager.cpp
@@ -1,7 +1,16 @@
 #include "OctreeManager.h"
 
 
-OctreeManager::OctreeManager() { }
+OctreeManager::OctreeManager(): rootNode(nullptr) { }
 
 
 OctreeManager::~OctreeManager() { }
+
+
+void OctreeManager::AddLeaf(const glm::vec3& pos) {
+	if (rootNode == nullptr) {
+		// add first node
+
+		//IDEA: "pos" ist northwest bottom corner, "center" is actual center position of node
+	}
+}
--- src/scene/OctreeManager.h
+++ src/scene/OctreeManager.h
@@ -1,12 +1,24 @@
 #pragma once
 #include <vector>
+#include <memory>
+#include <glm/glm.hpp>
 #include "OctreeNode.h"
 
 
 class OctreeManager final {
+private:
+	// IDEA: manager owns all nodes, independently of their actual structure
+	std::vector<std::unique_ptr<OctreeNode>> nodeInstances;
+
+public:
+	OctreeNode* rootNode;
+
 public:
 	OctreeManager();
+
 	~OctreeManager();
 
+
+	void AddLeaf(const glm::vec3& pos);
 };
 
