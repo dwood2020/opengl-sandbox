246a8b1c1115ea130b85151fd76057816531d85e
David <d.woodward@gmx.de>
2021-06-25 20:14:16 +0200

    added refs

--- src/renderer/VoxelRenderer.cpp
+++ src/renderer/VoxelRenderer.cpp
@@ -1,7 +1,8 @@
 #include "VoxelRenderer.h"
 
 
-VoxelRenderer::VoxelRenderer() { }
+VoxelRenderer::VoxelRenderer(Lighting& lighting, CameraBase& camera, VoxelScene& scene): 
+	lighting(&lighting), camera(&camera), scene(&scene) { }
 
 
 VoxelRenderer::~VoxelRenderer() { }
--- src/renderer/VoxelRenderer.h
+++ src/renderer/VoxelRenderer.h
@@ -1,10 +1,21 @@
 #pragma once
 #include "RendererBase.h"
+#include "../Lighting.h"
+#include "../camera/CameraBase.h"
+#include "../voxel/VoxelScene.h"
 
 
 class VoxelRenderer final : public RendererBase {
+private:
+	Lighting* lighting;
+	CameraBase* camera;
+	VoxelScene* scene;
+
+
+	VoxelRenderer() = default;
+
 public:
-	VoxelRenderer();
+	VoxelRenderer(Lighting& lighting, CameraBase& camera, VoxelScene& scene);
 
 	~VoxelRenderer();
 
