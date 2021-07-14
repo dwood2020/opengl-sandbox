7f73dd687e9bdeabdbd0199420ff0a4ccc1dcfb5
David <d.woodward@gmx.de>
2021-07-14 17:11:25 +0200

    rmd friend

--- src/renderer/SimpleRenderCommand.hpp
+++ src/renderer/SimpleRenderCommand.hpp
@@ -15,13 +15,16 @@ private:
 public:
 	SimpleRenderCommand(): M(glm::mat4(1.0f)), mesh(nullptr), material(nullptr), isActive(true) { }
 
+
 	SimpleRenderCommand(const glm::mat4& M, MeshBase* mesh, MaterialBase* material):
 		M(M), mesh(mesh), material(material), isActive(true) { }
 
+
 	glm::mat4& GetModelMatrix(void) { return M; }	
+
 
 	void SetActiveState(bool isActive) { this->isActive = isActive; }
 
-	friend class Renderer;
-	friend class MouseSelector;
+
+	friend class Renderer;	
 };
