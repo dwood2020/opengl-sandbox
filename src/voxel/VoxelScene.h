8b9132f7b0c0bafcd8b6deffe6bcc2792273074e
David <d.woodward@gmx.de>
2021-07-26 19:19:48 +0200

    Added comments

--- src/voxel/VoxelScene.h
+++ src/voxel/VoxelScene.h
@@ -10,6 +10,10 @@
 using SectionsMap = std::map<std::array<int, 3>, std::unique_ptr<Section>>;
 
 
+/// <summary>
+/// This class holds all Voxel sections and therefore represents the Voxel "Scene".
+/// It provides direct access to a single block.
+/// </summary>
 class VoxelScene final {
 private:
 	SectionsMap sections;	
@@ -46,9 +50,19 @@ public:
 	//TODO: Make this private and event-triggerable!
 	void GenerateMeshes(void);
 
-private:		
+private:
+	/// <summary>
+	/// Calculates the section position to any given block position
+	/// </summary>
+	/// <param name="pos">Block position</param>
+	/// <returns>Section position</returns>
 	std::array<int, 3> CalcSectionPosKey(const glm::ivec3& pos);	//TODO: use glm::ivec3 (needs custom serialization)
 
+	/// <summary>
+	/// Transforms global block position to a local section position
+	/// </summary>
+	/// <param name="pos">Global block position</param>
+	/// <returns>Local block position (in section)</returns>
 	glm::ivec3 ToSectionCoords(const glm::ivec3& pos);
 
 };
