f7defc960009df1bc001f9badc4efe58d3a15efa
dwood <d.woodward@gmx.de>
2021-09-03 18:54:42 +0200

    added getters7setters + started mesh gen method for voxelplane

--- src/voxel/VoxelPlane.cpp
+++ src/voxel/VoxelPlane.cpp
@@ -1,7 +1,51 @@
 #include "VoxelPlane.h"
 
 
-VoxelPlane::VoxelPlane(): origin(glm::ivec3()), orientation(glm::ivec2()) { }
+VoxelPlane::VoxelPlane(): origin(glm::ivec3()), orientation(glm::ivec2()), meshesAreDirty(false) { }
 
 
 VoxelPlane::~VoxelPlane() { }
+
+
+const glm::ivec3& VoxelPlane::GetOrigin(void) const {
+	return origin;
+}
+
+
+void VoxelPlane::SetOrigin(const glm::ivec3& origin) {
+	this->origin = origin;
+}
+
+
+const glm::ivec2& VoxelPlane::GetOrientation(void) const {
+	return orientation;
+}
+
+
+void VoxelPlane::SetOrientation(const glm::ivec2& orientation) {
+	this->orientation = orientation;
+}
+
+
+const glm::ivec2& VoxelPlane::GetDimension(void) const {
+	return dimension;
+}
+
+
+void VoxelPlane::SetDimension(const glm::ivec2& dimension) {
+	this->dimension = dimension;
+}
+
+
+std::array<DynamicMesh, 2>& VoxelPlane::GetMeshes(void) {
+	return meshes;
+}
+
+
+void VoxelPlane::GenerateMeshes(void) {
+	// origin cross as first render try:
+
+	auto& meshOriginVec = meshes[0].GetVerticesPos();
+	meshOriginVec.push_back({ glm::vec3() });
+	//TODO: implement this.
+}
--- src/voxel/VoxelPlane.h
+++ src/voxel/VoxelPlane.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <array>
 #include <glm/glm.hpp>
+#include "../mesh/DynamicMesh.h"
 
 
 /// <summary>
@@ -10,10 +12,44 @@ class VoxelPlane final {
 private:
 	glm::ivec3 origin;
 	glm::ivec2 orientation;
+	glm::ivec2 dimension;
+
+	std::array<DynamicMesh, 2> meshes;
+
+	bool meshesAreDirty;
 
 public:
+	/// <summary>
+	/// Constructor.
+	/// </summary>
 	VoxelPlane();
 
 	~VoxelPlane();
+
+
+	const glm::ivec3& GetOrigin(void) const;
+
+	void SetOrigin(const glm::ivec3& origin);
+
+	const glm::ivec2& GetOrientation(void) const;
+
+	void SetOrientation(const glm::ivec2& orientation);
+
+	const glm::ivec2& GetDimension(void) const;
+
+	void SetDimension(const glm::ivec2& dimension);
+
+
+	/// <summary>
+	/// Gets the meshes array.
+	/// </summary>
+	/// <returns>Meshes array</returns>
+	std::array<DynamicMesh, 2>& GetMeshes(void);
+
+
+	/// <summary>
+	/// (Re-)Generates the meshes.
+	/// </summary>
+	void GenerateMeshes(void);
 
 };
